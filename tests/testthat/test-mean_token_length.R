1160d1026b8ab8f2e298594bf1c1500de739f07f
Doug Friedman <sillyville+github@gmail.com>
2019-08-18 22:21:05 -0400

    tidying up the tests

--- tests/testthat/test-mean_token_length.R
+++ tests/testthat/test-mean_token_length.R
@@ -1,17 +1,17 @@
 # Test LDA VEM model from topicmodels
 test_that("LDA VEM", {
-  lda_vem = readRDS("test_data/test_lda_vem.RDS")
+  lda_vem <- readRDS("test_data/test_lda_vem.RDS")
   expect_equal(mean_token_length(lda_vem), c(5.1, 6.4, 5.3))
 })
 
 # Test LDA Gibbs model from topicmodels
 test_that("LDA Gibbs", {
-  lda_gibbs = readRDS("test_data/test_lda_gibbs.RDS")
+  lda_gibbs <- readRDS("test_data/test_lda_gibbs.RDS")
   expect_equal(mean_token_length(lda_gibbs), c(6.1, 5.8, 4.6))
 })
 
 # Test CTM VEM model from topicmodels
 test_that("CTM VEM", {
-  ctm_vem = readRDS("test_data/test_ctm_vem.RDS")
+  ctm_vem <- readRDS("test_data/test_ctm_vem.RDS")
   expect_equal(mean_token_length(ctm_vem), c(5.2, 7.0, 4.6))
 })
--- tests/testthat/test-topic_size.R
+++ tests/testthat/test-topic_size.R
@@ -1,20 +1,20 @@
 # Test LDA VEM model from topicmodels
 test_that("LDA VEM", {
-  lda_vem = readRDS("test_data/test_lda_vem.RDS")
+  lda_vem <- readRDS("test_data/test_lda_vem.RDS")
   expect_equal(topic_size(lda_vem), c(3432.904, 3397.258, 3642.838),
                tolerance = 1e-5)
 })
 
 # Test LDA Gibbs model from topicmodels
 test_that("LDA Gibbs", {
-  lda_gibbs = readRDS("test_data/test_lda_gibbs.RDS")
+  lda_gibbs <- readRDS("test_data/test_lda_gibbs.RDS")
   expect_equal(topic_size(lda_gibbs), c(3562.351, 3291.167, 3619.483),
                tolerance = 1e-5)
 })
 
 # Test CTM VEM model from topicmodels
 test_that("CTM VEM", {
-  ctm_vem = readRDS("test_data/test_ctm_vem.RDS")
+  ctm_vem <- readRDS("test_data/test_ctm_vem.RDS")
   expect_equal(topic_size(ctm_vem), c(589.6001, 529.5149, 708.8850),
                tolerance = 1e-5)
 })
